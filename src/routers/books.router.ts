0db273460ea8af744249bb68bd4672a715b41227
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-23 15:12:35 -0300

    :sparkles:	feat:  books  routers

--- src/routers/books.router.ts
+++ src/routers/books.router.ts
@@ -6,13 +6,17 @@ import {
   postBooksController,
   updateBooksController,
 } from "../controllers/books.controller.js";
+import { idBooksMiddlewares } from "../middlewares/books.middlewares.js";
+import postBooksSchema from "../schemas/books.schemas.js";
+import { validateSchema } from "../schemas/shemaValidation.js";
 
 const router = Router();
 
 router.get("/books", getBooksController);
-router.post("/books", postBooksController);
-router.put("/books/:id", updateBooksController);
-router.delete("/books/:id", deleteBooksController);
-router.get("/books/all", allBooksController);
+router.post("/books", validateSchema(postBooksSchema), postBooksController);
+router.put("/books/:id",idBooksMiddlewares, validateSchema(postBooksSchema), updateBooksController);
+router.delete("/books/:id", idBooksMiddlewares, deleteBooksController);
+router.get("/books/all/:fil", allBooksController);
+ 
 
 export default router;
