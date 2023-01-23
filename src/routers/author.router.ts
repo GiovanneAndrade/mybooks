a94a8459301e29e2d9f35994db4bc562e7f0bf69
GiovanneAndrade <vanne.as@hotmail.com>
2023-01-23 15:12:12 -0300

    :sparkles:	feat: post authors  routers

--- src/routers/author.router.ts
+++ src/routers/author.router.ts
@@ -1,14 +1,15 @@
-import { Router } from "express"
-import { getAuthorController, postAuthorController } from "../controllers/author.controller.js"
- 
- 
+import { Request, Response, Router } from "express";
+import {
+  getAuthorController,
+  postAuthorController,
+} from "../controllers/author.controller.js";
+import postAuthorSchema from "../schemas/author.schema.js";
+import { validateSchema } from "../schemas/shemaValidation.js";
+
+const router = Router();
+
+router.get("/author",  getAuthorController);
+router.post("/author", validateSchema(postAuthorSchema), postAuthorController);
  
 
-const router = Router()
-
-router.get('/author', getAuthorController)
-router.post('/author', postAuthorController)
- 
-
-   
-export default router
+export default router;
